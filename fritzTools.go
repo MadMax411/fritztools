2278664d791beb3b5aa190b51866770617fefd0c
Markus Neugebauer <markus@newgefarmer.de>
2015-04-06 21:38:54 +0200

    Config and SMTP-Mail

--- fritzTools.go
+++ fritzTools.go
@@ -2,26 +2,76 @@ package main
 
 import (
 	"fmt"
+	"net/mail"
 	"net/textproto"
+	"net/smtp"
+	"encoding/base64"
 	"strings"
+	"log"
+	"code.google.com/p/gcfg"
 )
 
-func main() {
-	host := "router.home.lan"
-	port := "1012"
+type lineHandler struct {
+    cn *textproto.Conn
+}
 
-	conn, err := textproto.Dial("tcp", host+":"+port)
-	defer conn.Close()
-	if err != nil {
-		panic(1)
+func SendMail( subject string, mailtext string ) {
+	var cfg MainConfig
+    err := gcfg.ReadFileInto( &cfg, "/home/markus/gocode/src/github.com/madmax411/fritztools/fritzTools.gcfg")
+    if err != nil {
+        log.Fatal(err)
+    }    
+
+    smtpServer := cfg.SMTP.Host
+	auth := smtp.PlainAuth(
+		"",
+		cfg.SMTP.User,
+		cfg.SMTP.Password,
+		smtpServer,
+	)
+ 
+	from := mail.Address{"", cfg.Mail.From}
+	to := mail.Address{"", cfg.Mail.To}
+	title := subject
+ 
+	body := mailtext
+ 
+	header := make(map[string]string)
+	header["From"] = from.String()
+	header["To"] = to.String()
+	header["Subject"] = title
+	header["MIME-Version"] = "1.0"
+	header["Content-Type"] = "text/plain; charset=\"utf-8\""
+	header["Content-Transfer-Encoding"] = "base64"
+ 
+	message := ""
+	for k, v := range header {
+		message += fmt.Sprintf("%s: %s\r\n", k, v)
 	}
-	fmt.Println("Connected to", host)
+	message += "\r\n" + base64.StdEncoding.EncodeToString([]byte(body))
+ 
+	// Connect to the server, authenticate, set the sender and recipient,
+	// and send the email all in one step.
+	errmail := smtp.SendMail(
+		smtpServer + ":" + cfg.SMTP.Port,
+		auth,
+		from.Address,
+		[]string{to.Address},
+		[]byte(message),
+		//[]byte("This is the email body."),
+	)
+	if errmail != nil {
+		log.Fatal(errmail)
+	}
+}
 
-	lastAction := ""
+func (l *lineHandler) Watch() {
+    lastAction := ""
 	currAction := ""
+    lastCallNo := ""
 
 	for {
-		line, err := conn.Reader.ReadLine()
+		line, err := l.cn.Reader.ReadLine()
 		if err != nil {
 			panic(2)
 		}
@@ -32,11 +82,13 @@ func main() {
 		switch currAction {
 		case "RING":
 			fmt.Println("Call from " + callValues[3])
+			lastCallNo = callValues[3]
 		case "CONNECT":
 			fmt.Println("Connected with extention station #" + callValues[3])
 		case "DISCONNECT":
 			if lastAction == "RING" {
-				fmt.Println("TODO: Send a info mail...")
+				fmt.Println("Send a info mail...")
+				SendMail( "Fritz: Call", "Call from " + lastCallNo )
 			}
 
 			fmt.Println("Disconneted")
@@ -44,4 +96,57 @@ func main() {
 
 		lastAction = currAction
 	}
+	
+	return   
 }
+
+type MainConfig struct {
+    Fritzbox Config_Fritzbox
+    SMTP Config_SMTP
+    Mail Config_Mail    
+}
+
+type Config_Fritzbox struct {
+    Host string
+    Port string
+}
+
+type Config_SMTP struct {
+    Host string
+    Port string
+    User string
+    Password string
+}
+
+type Config_Mail struct {
+    From string
+    To string
+}
+
+
+
+
+func main() {
+	var cfg MainConfig
+    err := gcfg.ReadFileInto( &cfg, "/home/markus/gocode/src/github.com/madmax411/fritztools/fritzTools.gcfg")
+    if err != nil {
+        log.Fatal(err)
+    }    
+   
+    SendMail( "Fritz", "Test" )
+   
+    conn, err := textproto.Dial("tcp", cfg.Fritzbox.Host + ":" + cfg.Fritzbox.Port)
+	defer conn.Close()
+	if err != nil {
+		panic(1)
+	}
+	
+	fmt.Println("Connected to", cfg.Fritzbox.Host)
+   
+    lh := lineHandler{}
+    lh.cn = conn
+
+    lh.Watch()
+}
+
+
