3ef4975112f7dc8281d386bae7ed432f9c52fa85
Markus Neugebauer <markus@newgefarmer.de>
2015-04-09 22:52:55 +0200

    Handling answering maschine added

--- fritzTools.go
+++ fritzTools.go
@@ -1,65 +1,65 @@
 package main
 
 import (
-	"fmt"
-	"net/mail"
-	"net/textproto"
-	"net/smtp"
-	"encoding/base64"
-	"strings"
-	"log"
 	"code.google.com/p/gcfg"
+	"encoding/base64"
+	"fmt"
+	"log"
+	"net/mail"
+	"net/smtp"
+	"net/textproto"
+	"strings"
 )
 
 type lineHandler struct {
-    cn *textproto.Conn
-    cfg MainConfig
+	cn  *textproto.Conn
+	cfg MainConfig
 }
 
 type lastCall struct {
-    PhoneNo string
-    DateTime string
+	PhoneNo  string
+	DateTime string
 }
 
 type MainConfig struct {
-    Fritzbox Config_Fritzbox
-    SMTP Config_SMTP
-    Mail Config_Mail    
+	Fritzbox Config_Fritzbox
+	SMTP     Config_SMTP
+	Mail     Config_Mail
 }
 
 type Config_Fritzbox struct {
-    Host string
-    Port string
+	Host string
+	Port string
 }
 
 type Config_SMTP struct {
-    Host string
-    Port string
-    User string
-    Password string
+	Host     string
+	Port     string
+	User     string
+	Password string
 }
 
 type Config_Mail struct {
-    From string
-    To string
+	From string
+	To   string
 }
 
-func SendMail( subject string, mailtext string, cfg MainConfig ) {
-   
-    smtpServer := cfg.SMTP.Host
+func SendMail(subject string, mailtext string, cfg MainConfig) {
+
+	smtpServer := cfg.SMTP.Host
 	auth := smtp.PlainAuth(
 		"",
 		cfg.SMTP.User,
 		cfg.SMTP.Password,
 		smtpServer,
 	)
- 
+
 	from := mail.Address{"", cfg.Mail.From}
 	to := mail.Address{"", cfg.Mail.To}
 	title := subject
- 
+
 	body := mailtext
- 
+
 	header := make(map[string]string)
 	header["From"] = from.String()
 	header["To"] = to.String()
@@ -67,15 +67,15 @@ func SendMail( subject string, mailtext string, cfg MainConfig ) {
 	header["MIME-Version"] = "1.0"
 	header["Content-Type"] = "text/plain; charset=\"utf-8\""
 	header["Content-Transfer-Encoding"] = "base64"
- 
+
 	message := ""
 	for k, v := range header {
 		message += fmt.Sprintf("%s: %s\r\n", k, v)
 	}
 	message += "\r\n" + base64.StdEncoding.EncodeToString([]byte(body))
- 
+
 	errmail := smtp.SendMail(
-		smtpServer + ":" + cfg.SMTP.Port,
+		smtpServer+":"+cfg.SMTP.Port,
 		auth,
 		from.Address,
 		[]string{to.Address},
@@ -87,15 +87,16 @@ func SendMail( subject string, mailtext string, cfg MainConfig ) {
 }
 
 func (l *lineHandler) Watch() {
-    lastAction := ""
+	lastAction := ""
 	currAction := ""
+	extStation := ""
 
-    call := lastCall{}
+	call := lastCall{}
 
 	for {
 		line, err := l.cn.Reader.ReadLine()
 		if err != nil {
-			panic(2)
+			panic(err)
 		}
 
 		callValues := strings.Split(line, ";")
@@ -103,49 +104,59 @@ func (l *lineHandler) Watch() {
 
 		switch currAction {
 		case "RING":
-			fmt.Println("Call from " + callValues[3])
+			//fmt.Println("Call from " + callValues[3])
 			call.PhoneNo = callValues[3]
 			call.DateTime = callValues[0]
+
 		case "CONNECT":
-			fmt.Println("Connected with extention station #" + callValues[3])
+			//fmt.Println("Connected with extention station #" + callValues[3])
+			extStation = callValues[3]
+
 		case "DISCONNECT":
 			if lastAction == "RING" {
-				fmt.Print("Send a info mail...")
-				SendMail( "Fritz: Call", "Call from " + call.PhoneNo + " at " + call.DateTime, l.cfg )
-				fmt.Println("Ok")
+				//fmt.Print("Send a info mail...")
+
+				msg := "Unanswered call from " + call.PhoneNo + " at " + call.DateTime
+
+				SendMail("Fritz: Call", msg, l.cfg)
+				//fmt.Println("Ok")
 			}
 
-			fmt.Println("Disconneted")
+			if lastAction == "CONNECT" && extStation == "40" {
+				//fmt.Print("Send a info mail...")
+
+				msg := "Call from " + call.PhoneNo + " at " + call.DateTime
+				msg += "\nCall is answered by the answering machine"
+
+				SendMail("Fritz: Call", msg, l.cfg)
+				//fmt.Println("Ok")
+			}
+			//fmt.Println("Disconneted")
 		}
 
 		lastAction = currAction
 	}
-	
-	return   
-}
 
+	return
+}
 
 func main() {
 	var cfg MainConfig
-    err := gcfg.ReadFileInto( &cfg, "fritzTools.gcfg")
-    if err != nil {
-        log.Fatal(err)
-        panic(1)
-    }    
-   
-    conn, err := textproto.Dial("tcp", cfg.Fritzbox.Host + ":" + cfg.Fritzbox.Port)
+	err := gcfg.ReadFileInto(&cfg, "fritzTools.gcfg")
+	if err != nil {
+		log.Fatal(err)
+		panic(err)
+	}
+
+	conn, err := textproto.Dial("tcp", cfg.Fritzbox.Host+":"+cfg.Fritzbox.Port)
 	defer conn.Close()
 	if err != nil {
-		panic(1)
+		panic(err)
 	}
-	
-	fmt.Println("Connected to", cfg.Fritzbox.Host)
-   
-    lh := lineHandler{}
-    lh.cn = conn
-    lh.cfg = cfg
 
-    lh.Watch()
+	lh := lineHandler{}
+	lh.cn = conn
+	lh.cfg = cfg
+
+	lh.Watch()
 }
-
-
